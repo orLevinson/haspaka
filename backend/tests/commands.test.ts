bb729fac9d8e1e75b4471678021ff0f420ef9ce5
Or Levinson <or20022@gmail.com>
2023-11-08 20:31:13 +0200

    added items routes

--- backend/tests/commands.test.ts
+++ backend/tests/commands.test.ts
@@ -1,9 +1,20 @@
-// import the testing files
-import request from "supertest";
-import app from "../src/app";
+// get the testing stuff
+import request from "supertest"
+import { describe, it } from "node:test";
 
 // get envs
 import dotenv from "dotenv"
 dotenv.config({path: `.env.${process.env.NODE_ENV}`})
 
+// import app
+import app from "../src/app";
 
+// describe("GET /api/products/:id", () => {
+//     it("should return a product", async () => {
+//       const res = await request(app).get(
+//         "/api/products/6331abc9e9ececcc2d449e44"
+//       );
+//       expect(res.statusCode).toBe(200);
+//       expect(res.body.name).toBe("Product 1");
+//     });
+//   });
