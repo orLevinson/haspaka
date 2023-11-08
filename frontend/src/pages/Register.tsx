df0d64656a79bde35b5bf6ccb9ca72988994d61f
Matan <matan1111@gmail.com>
2023-11-08 12:49:57 +0200

    login works, only need to add jwt to context

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,11 +1,57 @@
-import { Navigate } from "react-router";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
 
-    localStorage.setItem('jwt', 'abcde');
-    window.dispatchEvent(new Event('storage'));
+    // localStorage.setItem('jwt', 'abcde');
+    // window.dispatchEvent(new Event('storage'));
+    // return <Navigate to='/units' />
 
-    return <Navigate to='/units' />
+    const [name, setName] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        if (value >>> 0 === parseFloat(value) || value === '') setUsername(value);
+    }
+
+    const handleSubmit = () => axios.post(import.meta.env.VITE_REACT_APP_BASE_URL + "/users/register", { name, username, password })
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+
+    return (
+        <section className="bg-gray-50 dark:bg-gray-900 h-full" dir="rtl">
+            <div className="flex flex-col items-center justify-center px-6 mx-auto h-full lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            הרשמה למערכת
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                            <div>
+                                <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">שם</label>
+                                <input value={name} onChange={e => setName(e.target.value)} type="text" name="name" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="לדוג' יוסי כהן" required="" />
+                            </div>
+                            <div>
+                                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">מספר אישי</label>
+                                <input value={username} onChange={handleUsernameChange} type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="לדוג' 1234567" required="" />
+                            </div>
+                            <div>
+                                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">סיסמה</label>
+                                <input value={password} onChange={e => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            </div>
+                            <button type="submit" className="w-full text-white bg-teal-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">הרשמה</button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                יש לך משתמש? <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">התחברות</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Register;
