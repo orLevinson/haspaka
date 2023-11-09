fc9477bd46beb84014499e9965a75c23518d5c58
Matan <matan1111@gmail.com>
2023-11-09 17:08:47 +0200

    fixed login width

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -26,7 +26,7 @@ const Login = () => {
     return (
         <section className="bg-gray-50 h-full" dir="rtl">
             <div className="flex flex-col items-center justify-center px-6 mx-auto h-full lg:py-0">
-                <div className="w-full bg-white rounded-lg shadow-lg">
+                <div className="w-full bg-white rounded-lg shadow-lg  md:mt-0 sm:max-w-md xl:p-0">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                             התחברות למערכת
