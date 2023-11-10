225d3e68f8e57df7faf959923fd174b1b73ada94
Or Levinson <or20022@gmail.com>
2023-11-10 15:46:16 +0200

    fixed login and register backgrounds

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -33,7 +33,7 @@ const Login = () => {
     };
 
     return (
-        <section className="bg-gray-50 h-full" dir="rtl">
+        <section className="h-full" dir="rtl">
             <div className="flex flex-col items-center justify-center px-6 mx-auto h-full lg:py-0">
                 <div className="w-full bg-white rounded-lg shadow-lg  md:mt-0 sm:max-w-md xl:p-0">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
